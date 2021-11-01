a67d26bb1a2c0db9df57e4dc5e7bf54e66c5e910
James Hansen <jameszenartist@gmail.com>
2021-11-01 13:51:52 -0400

    fixed page loading issue & typing animation

--- preloader.js
+++ preloader.js
@@ -1,11 +1,22 @@
 window.addEventListener("load", () => {
   setTimeout(() => {
+    window.onload();
     let content = document.getElementById("home");
     let wrapper = document.querySelector(".wrapper");
     wrapper.classList.add("fadeaway");
     setTimeout(() => {
       document.body.removeChild(wrapper);
       content.classList.add(".fadein");
+      setTimeout(triggerAnim, 1000);
     }, 3000);
   }, 3000);
 });
+
+window.onload = function () {
+  window.scrollTo(0, 0);
+};
+
+function triggerAnim() {
+  document.querySelector("h1").style.opacity = 1;
+  return document.querySelector(".home").children[0].classList.add("type-me");
+}
